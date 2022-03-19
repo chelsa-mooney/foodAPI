b4b1f1e697d5c5a5f0d8cdee25de473994c56bea
chelsamooney <97555525+chelsa-mooney@users.noreply.github.com>
2022-03-19 14:00:06 -0400

    pprint

--- foodAPI.py
+++ foodAPI.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 #keys
 import requests
+from pprint import pprint
 import spoonacular as spoon
 apiKey = spoon.API("735810a590ed41ad81ca2a1ccbcd3703")
 
-#print("Hello!")
-#ingredients = input("Feed me a list of ingredients! ")
+print("Hello!")
+ingredients = input("Feed me a list of ingredients! ")
+
+headers = {
+    'content-type': "application/json",
+    }
+query = ingredients
+response = apiKey.search_recipes_by_ingredients(ingredients)
+pprint(response.json())
+print("Sound yummy? Yes! No, give me another recipe. ")
 
 
 #response = requests.get("https://api.spoonacular.com/recipes/complexSearch?apiKey=735810a590ed41ad81ca2a1ccbcd3703")
 #data = response.json()
 #print(data)
-
-headers = {
-    'content-type': "application/json",
-    }
-
-response = apiKey.get_a_random_food_joke()
-data = response.json()
-print(data['text'])
