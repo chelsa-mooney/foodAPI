00b79f7f79b1021f96df14272afbed37475bcbd5
chelsamooney <97555525+chelsa-mooney@users.noreply.github.com>
2022-03-19 14:52:10 -0400

    grr

--- foodAPI.py
+++ foodAPI.py
@@ -5,6 +5,9 @@ from pprint import pprint
 import spoonacular as spoon
 apiKey = spoon.API("735810a590ed41ad81ca2a1ccbcd3703")
 
+#food list
+food_list = []
+
 print("Hello!")
 ingredients = input("Feed me a list of ingredients! ")
 
@@ -14,7 +17,14 @@ headers = {
 query = ingredients
 response = apiKey.search_recipes_by_ingredients(ingredients)
 pprint(response.json())
-print("Sound yummy? Yes! No, give me another recipe. ")
+
+prompt1 = input("Sound yummy? Yes! No, give me another recipe. ").lower()
+if prompt1 == "yes":
+    food_list.append(ingredients)
+print(food_list)
+if prompt1 == "no":
+    ingredients = input("Feed me a list of ingredients! ")
+
 
 
 #response = requests.get("https://api.spoonacular.com/recipes/complexSearch?apiKey=735810a590ed41ad81ca2a1ccbcd3703")
