24cac9752565a6ac96a1abab8c2dc848cd7599d9
chelsamooney <97555525+chelsa-mooney@users.noreply.github.com>
2022-03-21 17:22:26 -0400

    attempt to mask api

--- foodAPI.py
+++ foodAPI.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-#keys
-import requests
+
 from pprint import pprint
 import spoonacular as spoon
+
 apiKey = spoon.API("735810a590ed41ad81ca2a1ccbcd3703")
+#apiKey = os.environ.get("apiKey")?
 
 headers = {
     'content-type': "application/json",
@@ -30,10 +31,11 @@ else:
     ingredients = input("Feed me a list of ingredients! ")
     new_func(apiKey, ingredients)
 
+
 print(food_list)
 
-#grocery store map of products
-def map_ingredients_to_grocery_products(food_list, servings):
-    query = "food/ingredients/map"
-    response = apiKey.map_ingredients_to_grocery_products(food_list)
-map_ingredients_to_grocery_products(food_list)
+#price breakdown
+#def visualize_price_breakdown(food_list, servings):
+    #query = "food/ingredients/map"
+    #response = apiKey.visualize_price_breakdown(food_list)
+#visualize_price_breakdown(food_list, servings)
